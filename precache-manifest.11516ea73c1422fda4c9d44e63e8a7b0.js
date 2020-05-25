2920a0892ba8c34d70ea9a9146f84d5bafe7047d
disko998 <disko998@gmail.com>
2020-05-25 21:51:30 +0200

    Updates

---
+++ precache-manifest.11516ea73c1422fda4c9d44e63e8a7b0.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7a00f8e55b5c4e1153a",
+    "url": "/disko998/Kasa/static/js/app.bebe0f1b.chunk.js"
+  },
+  {
+    "revision": "1ede849717e9631d1d7a",
+    "url": "/disko998/Kasa/static/js/runtime~app.13d97149.js"
+  },
+  {
+    "revision": "48660eafb61304202bd5",
+    "url": "/disko998/Kasa/static/js/2.b05824d9.chunk.js"
+  },
+  {
+    "revision": "004c2bbb035d8d06bb830efc4673c886",
+    "url": "/disko998/Kasa/static/media/star.004c2bbb.png"
+  },
+  {
+    "revision": "2327736b3ea09c41abfb69be1221f430",
+    "url": "/disko998/Kasa/static/media/heart.2327736b.png"
+  },
+  {
+    "revision": "51671417ef20e0bbc32f0a2bc6edfa95",
+    "url": "/disko998/Kasa/static/media/rocket.51671417.png"
+  },
+  {
+    "revision": "a7b9dc9de5f8f1fb1afbef917619a5ac",
+    "url": "/disko998/Kasa/static/media/bell.a7b9dc9d.png"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/disko998/Kasa/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "6beba7e6834963f7f171d3bdd075c915",
+    "url": "/disko998/Kasa/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/disko998/Kasa/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/disko998/Kasa/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/disko998/Kasa/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "5a293a273bee8d740a045d9922b9a9ae",
+    "url": "/disko998/Kasa/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/disko998/Kasa/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/disko998/Kasa/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/disko998/Kasa/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "49a79d66bdea2debf1832bf4d7aca127",
+    "url": "/disko998/Kasa/./fonts/SpaceMono-Regular.ttf"
+  },
+  {
+    "revision": "872545dde71de3842234bf6afe80c4cb",
+    "url": "/disko998/Kasa/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "c6aef942e3668158ec29d4adcb2e768f",
+    "url": "/disko998/Kasa/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/disko998/Kasa/expo-service-worker.js"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/disko998/Kasa/serve.json"
+  },
+  {
+    "revision": "683774508412ee16f2454e1e032822fd",
+    "url": "/disko998/Kasa/favicon-16.png"
+  },
+  {
+    "revision": "0e0c4796e6755ff4bb6e5235f708f31e",
+    "url": "/disko998/Kasa/favicon-32.png"
+  },
+  {
+    "revision": "eb38ff8bcb7094cbdad1411daf87cb79",
+    "url": "/disko998/Kasa/favicon.ico"
+  },
+  {
+    "revision": "b10092feca9a960d0be168dc3ddd969d",
+    "url": "/disko998/Kasa/index.html"
+  },
+  {
+    "revision": "fb52058fcb8d48e301dd1be701ef5821",
+    "url": "/disko998/Kasa/manifest.json"
+  },
+  {
+    "revision": "b27ed25dc012ca6d38408fb65b673d62",
+    "url": "/disko998/Kasa/register-service-worker.js"
+  }
+];
---
+++ register-service-worker.js
@@ -0,0 +1,14 @@
+/* eslint-env browser */
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker
+      .register('/disko998/Kasa/expo-service-worker.js', { scope: '/disko998/Kasa/' })
+      .then(function (info) {
+        // console.info('Registered service-worker', info);
+      })
+      .catch(function (error) {
+        console.info('Failed to register service-worker', error);
+      });
+  });
+}
